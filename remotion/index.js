1f0411e827e9d068b9482838299a0def0819cefd
MuhammadSaidulislam <mislam171222@bscse.uiu.ac.bd>
2026-03-24 22:14:34 +0600

    render add

--- remotion/index.js
+++ remotion/index.js
@@ -5,7 +5,7 @@ import { Composition } from "remotion";
 // No TypeScript types here
 export const MyVideo = ({ title = "Default Title" }) => {
   return (
-    <div style={{ flex: 1, backgroundColor: "black", color: "white" }}>
+    <div>
       <h1>{title}</h1>
     </div>
   );
